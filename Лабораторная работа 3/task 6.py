7315d616a0a75b2e5c319bdff3d787fe5f1c0010
ArtemPlotnikov1 <119523866+ArtemPlotnikov1@users.noreply.github.com>
2022-11-30 23:20:50 +0300

    Update task 6.py

--- Лабораторная работа 3/task 6.py
+++ Лабораторная работа 3/task 6.py
@@ -11,4 +11,4 @@ for i, current_number in enumerate(list_numbers):  # перебираем пар
 
 list_numbers[max_index], list_numbers[len(list_numbers)-1] = list_numbers[len(list_numbers)-1], list_numbers[max_index]
 
-print(list_numbers)
+print(list_numbers)  # получаем измененный список
